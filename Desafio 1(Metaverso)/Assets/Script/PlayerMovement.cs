2edaf9de84f7f7cda7c95e5cfa82c1ec37f7e972
JeilsonSilva <108960969+JeilsonSilva@users.noreply.github.com>
2022-08-25 22:00:33 -0300

    Dia 1
    Criação:
    Cenário
    Física

---
+++ Desafio 1(Metaverso)/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator Animation;
+    Rigidbody2D rig;
+    Transform trans;
+
+    [SerializeField] float speed, jumpForce, x, y;
+    [SerializeField] bool isJup, isDJump;
+    //[SerializeField] Text Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animation = GetComponent<Animator>();
+
+        rig = GetComponent<Rigidbody2D>();
+
+        trans = GetComponent<Transform>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Animations();
+        Move();
+        Jump();
+
+
+    }
+
+    private void Jump()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!isJup)
+            {
+                rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                isDJump = true;
+            }
+            else if (isDJump)
+            {
+                rig.AddForce(new Vector2(0, jumpForce * 1.5f), ForceMode2D.Impulse);
+                isDJump = false;
+
+            }
+        }
+
+    }
+
+    private void Move()
+    {
+
+        x = Input.GetAxisRaw("Horizontal") * speed;
+        y = rig.velocity.y;
+
+        rig.velocity = new Vector2(x, y);
+
+        if (rig.velocity.x > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (rig.velocity.x < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+    }
+
+    void Animations()
+    {
+
+        if (rig.velocity.x != 0 && !isJup) //run
+        {
+            Animation.SetFloat("anim", 0.25f);
+        }
+        else if (isDJump && rig.velocity.y > 0)//Jump
+        {
+            Animation.SetFloat("anim", 0.5f);
+        }
+        else if (!isDJump && rig.velocity.y > 0)//DJump
+        {
+            Animation.SetFloat("anim", 0.75f);
+        }
+        else if (rig.velocity.y < 0)//Fall
+        {
+            Animation.SetFloat("anim", 1f);
+        }
+        else if (rig.velocity.x == 0 && rig.velocity.x == 0)//idle
+        {
+            Animation.SetFloat("anim", 0f);
+        }
+
+
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isJup = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isJup = true;
+        }
+    }
+}
+

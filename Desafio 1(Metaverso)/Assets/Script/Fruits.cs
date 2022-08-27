62659ae12882d8366a5baa97e9fe4d03045cf777
JeilsonSilva <108960969+JeilsonSilva@users.noreply.github.com>
2022-08-26 21:54:22 -0300

    Implementação de frutas

---
+++ Desafio 1(Metaverso)/Assets/Script/Fruits.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruits : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private CircleCollider2D cir;
+    
+
+    public GameObject collected;
+    public int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        cir = GetComponent<CircleCollider2D>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            sr.enabled = false;
+            cir.enabled = false;
+            collected.SetActive(true);
+
+            GameController.instance.totalScore += Score;
+
+
+            Destroy(gameObject,0.25f);
+        }
+
+    }   
+}
